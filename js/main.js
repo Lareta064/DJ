7df1b84689e0ba7f3a35d718473908074d13dbca
Lareta <lareta064@gmail.com>
2018-08-17 17:57:54 +0300

    commit-1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+	highlightSelector:"nav a"
+	});
+
+	$('.timeline-item-link').on('click', function(e){
+		e.preventDefault();
+		$(this).siblings('.timeline-item-description').css('display','block');
+		$(this).parent('.timeline-item').siblings('.timeline-item').children('.timeline-item-description').css('display','none');
+	});
+})
