95108f3e6f8c296c35d409782cc94e875d49f099
Lareta <lareta064@gmail.com>
2018-08-17 18:17:25 +0300

    commit-3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-	$("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+	/*$("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
 	highlightSelector:"nav a"
-	});
+	});*/
 
 	$('.timeline-item-link').on('click', function(e){
 		e.preventDefault();
